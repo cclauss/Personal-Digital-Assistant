e78c6d98dd570298edb51a22f3fb4c38ae9259b9
Christian Clauss <cclauss@me.com>
2021-10-08 16:06:39 +0200

    Arrgg pytest

--- test_claussoft_pda.py
+++
@@ -1,5 +0,0 @@
-from .claussoft_pda import PDA
-
-
-def test_greeting() -> None:
-    assert PDA().greeting.startswith("Good")
---
+++ tests/test_claussoft_pda.py
@@ -0,0 +1,6 @@
+# from .claussoft_pda import PDA
+from claussoft_pda.claussoft_pda import PDA
+
+
+def test_greeting() -> None:
+    assert PDA().greeting.startswith("Good")
